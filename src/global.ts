8542bd0a360620e8c651a71f2816338a55897714
Paras Shah <parasshah195@gmail.com>
2024-03-24 20:15:35 +0530

    Header nav scroll interaction

--- src/global.ts
+++ src/global.ts
@@ -3,11 +3,13 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import { animatedDetailsAccordions } from './components/global/accordions';
 import { fadeUp } from './components/global/fade-up';
+import { initNav } from './components/global/header-nav';
 
 window.gsap = gsap;
 window.gsap.registerPlugin(ScrollTrigger);
 
 window.Webflow?.push(() => {
+  initNav();
   animatedDetailsAccordions();
   fadeUp();
 });
