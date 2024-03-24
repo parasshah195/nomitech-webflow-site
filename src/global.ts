f34047905f51a7ae5e13543d60c913b8cf2c3f3b
Paras Shah <parasshah195@gmail.com>
2024-03-24 22:10:28 +0530

    Set up fade-up data attributes on all bento grid components
    using the common component class name

--- src/global.ts
+++ src/global.ts
@@ -4,12 +4,21 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { animatedDetailsAccordions } from './components/global/accordions';
 import { fadeUp } from './components/global/fade-up';
 import { initNav } from './components/global/header-nav';
+import { SCRIPTS_LOADED_EVENT } from './constants';
 
 window.gsap = gsap;
 window.gsap.registerPlugin(ScrollTrigger);
 
 window.Webflow?.push(() => {
+  setFadeUpOnBentoGrid();
+
   initNav();
   animatedDetailsAccordions();
   fadeUp();
 });
+
+function setFadeUpOnBentoGrid() {
+  document.querySelectorAll('.bento-grid_component').forEach((item) => {
+    item.setAttribute('data-fade-up', 'stagger');
+  });
+}
