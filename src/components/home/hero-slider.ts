17ad28086342ab2f8b6628c458bd6dbc5bbb2070
Paras Shah <parasshah195@gmail.com>
2024-03-25 23:13:04 +0530

    Remove hero slider manual opacity implementation
    switch swiper-wrapper from absolute positioned to relative flex child; to work correctly in tandem with the `effect: fade`

--- src/components/home/hero-slider.ts
+++ src/components/home/hero-slider.ts
@@ -10,19 +10,6 @@ export function initHeroSlider() {
     fadeEffect: {
       crossFade: true,
     },
-    // effect: 'custom', // Custom effect to change background image
-    // customEffect: {
-    //   translate: function (swiper, current, next) {
-    //     var slides = swiper.slides;
-    //     for (var i = 0; i < slides.length; i++) {
-    //       if (i === current || i === next) {
-    //         slides[i].style.opacity = 1; // Show current and next slides
-    //       } else {
-    //         slides[i].style.opacity = 0; // Hide other slides
-    //       }
-    //     }
-    //   },
-    // },
     loop: true,
     slidesPerView: 1,
     lazyPreloadPrevNext: 1,
@@ -31,25 +18,5 @@ export function initHeroSlider() {
       delay: 5000,
     },
     speed: 1000,
-    // on: {
-    //   init: function () {
-    //     window.DEBUG('init');
-    //     resetSlideOpacity(this);
-    //   },
-    //   slideChangeTransitionStart: function () {
-    //     resetSlideOpacity(this);
-    //   },
-    // },
   });
-}
-
-function resetSlideOpacity(slider: Swiper) {
-  window.DEBUG('reset slide opacity');
-  // Set opacity for all slides to 0
-  slider.slides.forEach((slide) => {
-    slide.style.opacity = '0';
-  });
-
-  // Set opacity for active slide to 1
-  slider.slides[slider.activeIndex].style.opacity = '1';
 }
