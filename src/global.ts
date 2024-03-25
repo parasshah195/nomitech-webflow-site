9ca7eb59ec955405505c75eeed77e2ae047e5475
Paras Shah <parasshah195@gmail.com>
2024-03-25 13:15:52 +0530

    Remove unused constant

--- src/global.ts
+++ src/global.ts
@@ -4,7 +4,6 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { animatedDetailsAccordions } from './components/global/accordions';
 import { fadeUp } from './components/global/fade-up';
 import { initNav } from './components/global/header-nav';
-import { SCRIPTS_LOADED_EVENT } from './constants';
 
 window.gsap = gsap;
 window.gsap.registerPlugin(ScrollTrigger);
