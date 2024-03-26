edebfd22d1e2308a885cd0c9a6ca6fb7c124029f
Paras Shah <parasshah195@gmail.com>
2024-03-26 21:47:28 +0530

    Add home industry links hover interaction

---
+++ src/components/home/industries-links.ts
@@ -0,0 +1,76 @@
+const EL_ID_DATA_ATTRIBUTE = 'data-home-industries-el';
+const ATTR_VALUE_IMAGE = 'image-item';
+const ATTR_VALUE_LINK = 'link';
+
+const CUSTOM_CURSOR_SELECTOR = '.home-industries_links_cursor_component';
+const LINK_STANDARD_CURSOR_COMBO_CLASS = 'has-standard-cursor';
+
+const ACTIVE_CLASS = 'is-active';
+
+const customCursorEl = document.querySelector(CUSTOM_CURSOR_SELECTOR);
+
+let cursorMoveLastKnownPosition = { x: 0, y: 0 };
+let cursorMoveTicking = false;
+
+export function initHomeIndustriesLinks() {
+  const imageList = document.querySelectorAll(`[${EL_ID_DATA_ATTRIBUTE}="${ATTR_VALUE_IMAGE}"]`);
+  const linksList = document.querySelectorAll(`[${EL_ID_DATA_ATTRIBUTE}="${ATTR_VALUE_LINK}"]`);
+  let isCursorAvailable = true;
+
+  if (!imageList.length || !linksList.length) {
+    window.DEBUG('Home industries links or images not found', { imageList, linksList });
+    return;
+  }
+
+  if (!customCursorEl) {
+    window.DEBUG('Custom cursor element not found. Setting default cursor', { customCursorEl });
+    linksList.forEach((linkEl) => {
+      linkEl.classList.add(LINK_STANDARD_CURSOR_COMBO_CLASS);
+    });
+    isCursorAvailable = false;
+    return;
+  }
+
+  imageList[0].classList.add(ACTIVE_CLASS);
+
+  // On links hover, show the corresponding image, and hide the existing one
+  linksList.forEach((linkEl, position) => {
+    linkEl.addEventListener('mouseenter', () => {
+      window.DEBUG(`Industry link hover. Item ${position + 1}`);
+
+      imageList.forEach((imageEl) => {
+        imageEl.classList.remove(ACTIVE_CLASS);
+      });
+      imageList[position].classList.add(ACTIVE_CLASS);
+
+      customCursorEl.classList.add(ACTIVE_CLASS);
+
+      if (isCursorAvailable) {
+        window.addEventListener('mousemove', cursorMove);
+      }
+    });
+
+    linkEl.addEventListener('mouseleave', () => {
+      window.DEBUG(`Industry link hover out. Item ${position + 1}`);
+
+      customCursorEl.classList.remove(ACTIVE_CLASS);
+      if (isCursorAvailable) {
+        window.removeEventListener('mousemove', cursorMove);
+      }
+    });
+  });
+}
+
+function cursorMove(ev: MouseEvent) {
+  cursorMoveLastKnownPosition = { x: ev.clientX, y: ev.clientY };
+
+  if (!cursorMoveTicking) {
+    window.requestAnimationFrame(() => {
+      customCursorEl.style.left = `${cursorMoveLastKnownPosition.x}px`;
+      customCursorEl.style.top = `${cursorMoveLastKnownPosition.y}px`;
+      cursorMoveTicking = false;
+    });
+
+    cursorMoveTicking = true;
+  }
+}
--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,4 +1,5 @@
 import { initHeroSlider } from 'src/components/home/hero-slider';
+import { initHomeIndustriesLinks } from 'src/components/home/industries-links';
 import { initProductTabsAutoplay, setTabsResizeListener } from 'src/components/home/product-tabs';
 import { SCRIPTS_LOADED_EVENT } from 'src/constants';
 
@@ -13,4 +14,5 @@ window.fsAttributes.push([
 
 window.addEventListener(SCRIPTS_LOADED_EVENT, () => {
   initHeroSlider();
+  initHomeIndustriesLinks();
 });
