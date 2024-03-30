d1804aec638b7749feab36978d7de6e664b98650
Paras Shah <parasshah195@gmail.com>
2024-03-30 10:16:43 +0530

    Add more info and clarity to scripts environment info logging in console

--- src/dev/env.ts
+++ src/dev/env.ts
@@ -1,10 +1,14 @@
 import type { SCRIPTS_ENV } from 'global';
 
 const ENV_LOCALSTORAGE_ID = 'jsEnv';
+const ENV_NAMES = {
+  dev: 'Development',
+  prod: 'Production',
+};
 
 window.SCRIPTS_ENV = getENV();
 
-window.setScriptsENV = (env) => {
+window.setScriptsENV = (env: SCRIPTS_ENV) => {
   if (env !== 'dev' && env !== 'prod') {
     console.error('Invalid environment. Pass `dev` or `prod`');
     return;
@@ -12,7 +16,13 @@ window.setScriptsENV = (env) => {
 
   localStorage.setItem(ENV_LOCALSTORAGE_ID, env);
   window.SCRIPTS_ENV = env;
-  console.log(`Environment successfully set to ${env}`);
+
+  console.log(`Environment successfully set to ${ENV_NAMES[env]}`);
+  if ('prod' === env) {
+    console.log(
+      'To switch to dev mode and serve files from localhost, run `window.setScriptsENV("dev")` in the console'
+    );
+  }
 };
 
 function getENV(): SCRIPTS_ENV {
