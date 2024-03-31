7116f02ae9d16409ae58416a751e83e41ca3cb84
Paras Shah <parasshah195@gmail.com>
2024-03-31 18:04:29 +0530

    Build

---
+++ dist/prod/pages/blog.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{window.fsAttributes=window.fsAttributes||[];window.fsAttributes.push(["cmsfilter",()=>{let t=document.querySelector(".filter_button.is-filters"),e=$(t),s=".blog_filters-modal-controls";t==null||t.addEventListener("click",i=>{i.target.closest(s)&&(window.DEBUG("close"),e.trigger("w-close"))})}]);})();
